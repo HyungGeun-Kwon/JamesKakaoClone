89316a7ebadaf49a3af9bcebb4793ac62834e946
형근 권 <hgkwon@crevis.co.kr>
2023-03-31 12:26:40 +0900

    update

--- Kakao1.LayoutSupport/UI/Units/PlaceholderPasswordBox.cs
+++ Kakao1.LayoutSupport/UI/Units/PlaceholderPasswordBox.cs
@@ -18,5 +18,32 @@ namespace Kakao1.LayoutSupport.UI.Units
             get => (string)GetValue(PlaceholderTextProperty);
             set => SetValue(PlaceholderTextProperty, value);
         }
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            if (GetTemplateChild("PART_PasswordBox") is PasswordBox pwd)
+            {
+                pwd.PasswordChanged += Pwd_PasswordChanged;
+            }
+        }
+        private void Pwd_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (sender is PasswordBox pwd)
+            {
+                SetValue(TextProperty, pwd.Password);
+            }
+        }
+        private void CustomPasswordBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (e.OriginalSource == this)
+            {
+                PasswordBox pwbox = GetTemplateChild("PART_PasswordBox") as PasswordBox;
+                if(pwbox != null)
+                {
+                    pwbox.Focus();
+                }
+            }
+        }
     }
 }
